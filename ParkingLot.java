00a94c81e4e1a6da95f7529f9c589c9eaeeb0479
Alec Waichunas <awaichunas@gmail.com>
2019-10-22 14:02:46 -0500

    Added ParkingLot logic

--- ParkingLot.java
+++ ParkingLot.java
@@ -1,23 +1,31 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ParkingLot {
 	
-	public LotSection[] lotArray;
+	public ArrayList<LotSection> sections = new ArrayList<>();
 	public int lotId;
 
 	public ParkingLot(int lotId){
 		this.lotId = lotId;
 	}
 
-	public boolean addSection(LotSection lot){
-		return false;
+	public boolean addSection(LotSection section){
+		return sections.add(section);
 	}
 
-	public boolean removeSection(LotSection lot){
-		return false;
+	public boolean removeSection(LotSection section){
+		return sections.remove(section);
 	}
 
 	public LotSection getOpenLotSection(){
-		return null;
+		LotSection section = null;
+		for(int i = 0; i < sections.size(); i++){
+			if(sections.get(i).hasOpenSpots()){
+				section = sections.get(i);
+				break;
+			}
+		}
+		return section;
 	}
 }
