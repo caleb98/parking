31a7032cb9b503d8a2b08fb5b43afc6328dd2490
Alec Waichunas <awaichunas@gmail.com>
2019-10-22 14:14:12 -0500

    Main Testing Driver Added

--- Main.java
+++ Main.java
@@ -5,31 +5,56 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String args[]){
-        TicketManager ticketManager = new TicketManager();
+        ParkingLot parkingLot = new ParkingLot(1);
+        parkingLot.addSection(new LotSection("A", 5));
 
+        TicketManager ticketManager = new TicketManager();
+        ticketManager.addLot(parkingLot);
+
+        InputStreamReader isr = new InputStreamReader(System.in);
         while(true){
             System.out.println("1. Entering\n2.Leaving");
             int option = 0;
             try{
-            InputStreamReader isr = new InputStreamReader(System.in);
-            BufferedReader br = new BufferedReader(isr);
-            option = Integer.parseInt(br.readLine());
-            isr.close();
-            br.close();
+                BufferedReader br = new BufferedReader(isr);
+                option = Integer.parseInt(br.readLine());
             }catch(IOException e){
                 e.printStackTrace();
+                break;
             }
             switch(option){
                 case 1: 
-                    ticketManager.scanCard();
+                    Card card = ticketManager.scanCard();
+                    boolean success = ticketManager.startTransaction(card);
+                    if(success){
+                        ticketManager.setGateOpen();
+                        try{
+                            Thread.sleep(1000);
+                        }catch(InterruptedException e){
+                            e.printStackTrace();
+                        }
+                        ticketManager.setGateClosed();
+                    }else{
+                        System.out.println("An open spot could not be found!");
+                    }
                     break;
                 case 2:
-                    ticketManager.scanTicket();
+                    int ticketId = ticketManager.scanTicket();
+                    boolean tranasctionComplete = ticketManager.completeTransaction(ticketId);
+                    if(tranasctionComplete)
+                        System.out.println("Transaction has been completed");
+                    else
+                        System.out.println("Transaction failed, check ticked id");
                     break;
                 default:
                     System.out.println("Invalid Option");
             }
-
+            if(option==3) break;
+        }
+        try{
+            isr.close();
+        }catch(IOException e){
+            e.printStackTrace();
         }
     }
 
