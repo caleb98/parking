cef9c4ab8fac5ff2e19694f187cc2f6a594096b3
Alec Waichunas <awaichunas@gmail.com>
2019-10-22 13:42:21 -0500

    Added ending time to transaction

--- Transaction.java
+++ Transaction.java
@@ -5,10 +5,17 @@ public class Transaction {
 	public final LotSection lotUsed;
 	public final long timeCreatedInMS;
 	
+	public long timeEndedInMS = 0;
+	
 	public Transaction(int id, LotSection sectionUsed) {
 		transactionId = id;
 		lotUsed = sectionUsed;
 		timeCreatedInMS = System.currentTimeMillis();
 	}
 	
+	// return the time difference of the transaction
+	// if timeEnded is not up, get the differnce from now
+	public long getTimeDifferenceInMS(){
+		return ((timeEndedInMS == 0) ? System.currentTimeMillis() : timeEndedinMS) - timeCreatedInMS;
+	}
 }
