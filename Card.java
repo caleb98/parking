55e0f9a2f075a1f55c0c4202635e5fd4c0211564
donna-st-amour <donna.st.amour@gmail.com>
2019-10-24 12:01:21 -0500

    Update Card.java
    changed data type of cardNumber to long

--- Card.java
+++ Card.java
@@ -1,11 +1,11 @@
 public class Card {
 	
 	public final String name;
-	public final int cardNumber;
+	public final long cardNumber;
 	public final String expirationDate;
 	public final int securityCode;
 	
-	public Card(String name, int cardNumber, String expirationDate, int securityCode){
+	public Card(String name, long cardNumber, String expirationDate, int securityCode){
 		this.name = name;
 		this.cardNumber = cardNumber;
 		this.expirationDate = expirationDate;
