53fd18e154bab0b76c84f2e309e21a9380824be4
Alec <awaichunas@gmail.com>
2019-11-19 14:48:52 -0600

    Updated Transaction Test

--- src/main/java/test/TransactionTest.java
+++ src/main/java/test/TransactionTest.java
@@ -34,19 +34,33 @@ class TransactionTest {
 	@Test
 	@Order(2)
 	void tranasctionCreation(){
-        transcaction = new Transaction(0, lot.hourlyRate, lot, lot.getOpenLotSection());
+		lot = new ParkingLot(1, "North", 3.0f);
+        lot.addSection(new LotSection("A", 50));
+        lot.addSection(new LotSection("B", 25));
+		LotSection lotSection = lot.getOpenLotSection();
+        transcaction = new Transaction(0, lot.hourlyRate, lot, lotSection);
         assertTrue(transcaction != null);
     }
 
 	@Test
 	@Order(3)
 	void rateInitialization(){
+		lot = new ParkingLot(1, "North", 3.0f);
+        lot.addSection(new LotSection("A", 50));
+        lot.addSection(new LotSection("B", 25));
+		LotSection lotSection = lot.getOpenLotSection();
+        transcaction = new Transaction(0, lot.hourlyRate, lot, lotSection);
         assertEquals(transcaction.getHourlyRate(), lot.hourlyRate);
 	}
 
 	@Test
 	@Order(4)
 	void rateConsistency(){
+		lot = new ParkingLot(1, "North", 3.0f);
+        lot.addSection(new LotSection("A", 50));
+        lot.addSection(new LotSection("B", 25));
+		LotSection lotSection = lot.getOpenLotSection();
+        transcaction = new Transaction(0, lot.hourlyRate, lot, lotSection);
         double savedRate = transcaction.getHourlyRate();
         lot.hourlyRate = 3.5f;
         assertEquals(savedRate, transcaction.getHourlyRate());
@@ -56,6 +70,11 @@ class TransactionTest {
 	@Test
 	@Order(5)
 	void totalCalculated(){
+		lot = new ParkingLot(1, "North", 3.0f);
+        lot.addSection(new LotSection("A", 50));
+        lot.addSection(new LotSection("B", 25));
+		LotSection lotSection = lot.getOpenLotSection();
+        transcaction = new Transaction(0, lot.hourlyRate, lot, lotSection);
         transcaction.timeEnteredInMS = transcaction.timeEnteredInMS - (1000 * 60 * 60 * 3);
         transcaction.closeTransaction();
         assertTrue(transcaction.getTotalCost() > 0);
