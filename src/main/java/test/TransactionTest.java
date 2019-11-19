0e9857950d81aeb60230d71eebe77348240c814b
Alec Waichunas <awaichunas@gmail.com>
2019-11-19 11:44:48 -0600

    Added Transaction Test

---
+++ src/main/java/test/TransactionTest.java
@@ -0,0 +1,65 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import data.ParkingLot;
+import data.LotSection;
+import data.TicketManager;
+import data.Transaction;
+
+@TestMethodOrder(OrderAnnotation.class)
+class TransactionTest {
+
+	private TicketManager manager;
+	private ParkingLot lot;
+	private Transaction transcaction;
+
+	@Test
+	@Order(1)
+	void testLotCreation(){
+        lot = new ParkingLot(1, "North", 3.0f);
+        lot.addSection(new LotSection("A", 50));
+        lot.addSection(new LotSection("B", 25));
+        
+        assertTrue(lot != null);
+        assertTrue(lot.getAllSections().size() > 0);
+	}
+
+	@Test
+	@Order(2)
+	void tranasctionCreation(){
+        transcaction = new Transaction(0, lot.hourlyRate, lot.getOpenLotSection());
+        assertTrue(transcaction != null);
+    }
+
+	@Test
+	@Order(3)
+	void rateInitialization(){
+        assertEquals(transcaction.getHourlyRate(), lot.hourlyRate);
+	}
+
+	@Test
+	@Order(4)
+	void rateConsistency(){
+        double savedRate = transcaction.getHourlyRate();
+        lot.hourlyRate = 3.5f;
+        assertEquals(savedRate, transcaction.getHourlyRate());
+        assertTrue(transcaction.getHourlyRate() != lot.hourlyRate);
+    }
+
+	@Test
+	@Order(5)
+	void totalCalculated(){
+        transcaction.timeEnteredInMS = transcaction.timeEnteredInMS - (1000 * 60 * 60 * 3);
+        transcaction.closeTransaction();
+        assertTrue(transcaction.getTotalCost() > 0);
+    }
+
+}
