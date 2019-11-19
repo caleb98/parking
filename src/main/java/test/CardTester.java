3eb121b83ed7ca0e1f1498b2510e674005c2db96
donna-st-amour <donna.st.amour@gmail.com>
2019-11-19 15:39:04 -0600

    Update CardTester.java
    Altered testing to match with updates in the card class.

--- src/main/java/test/CardTester.java
+++ src/main/java/test/CardTester.java
@@ -1,4 +1,4 @@
-package test;
+package Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,49 +16,64 @@ class CardTester {
 	public static final String expDateTwo = "1119";
 	public static final String goodExpDate = "1120";
 	public static final String goodExpDateTwo = "1219";
-	public static final int secCodeOne = 23;
-	public static final int secCodeTwo = 5342;
-	public static final int goodSecCode = 345;
+	public static final String secCodeOne = "23";
+	public static final String secCodeTwo = "5342";
+	public static final String goodSecCode = "345";
+	public static final String alphaSecCode = "abc";
 	
 	
 	@Test
 	void cardLongNumberTest(Card card){
+		@SuppressWarnings("unused")
 		Card longNumber = new Card(name, longCardNumber, goodExpDate, goodSecCode);
 		assertTrue(Card.reject);
 	}//end cardLongNumberTest
 	
 	@Test
 	void cardShortNumberTest(Card card) {
+		@SuppressWarnings("unused")
 		Card shortNumber = new Card(name, shortCardNumber, goodExpDate, goodSecCode);
 		assertTrue(Card.reject);
 	}//end cardShortNumberTest
 	
 	@Test
 	void badExpOneTest(Card card) {
+		@SuppressWarnings("unused")
 		Card badExpOne = new Card(name, goodCardNumber, expDateOne, goodSecCode);
 		assertTrue(Card.reject);
 	}//end badExpOneTest		
 		
 	@Test
 	void badExpTwoTest(Card card) {
+		@SuppressWarnings("unused")
 		Card badExpTwo = new Card(name, goodCardNumber, expDateTwo, goodSecCode);
 		assertTrue(Card.reject);
 	}//end badExpTwoTest
 	
 	@Test
 	void badSecCodeOneTest(Card card) {
+		@SuppressWarnings("unused")
 		Card badSecOne = new Card(name, goodCardNumber, goodExpDate, secCodeOne);
 		assertTrue(Card.reject); 
 	}//end badSecCodeOneTest
 	
 	@Test
 	void badSecCodeTwoTest(Card card) { 
+		@SuppressWarnings("unused")
 		Card badSecTwo = new Card(name, goodCardNumber, goodExpDate, secCodeTwo);
 		assertTrue(Card.reject);
 	}//end badSecCodeTwoTest
 	
 	@Test
+	void alphaSecCodeTest(Card card) { 
+		@SuppressWarnings("unused")
+		Card badSecTwo = new Card(name, goodCardNumber, goodExpDate, alphaSecCode);
+		assertTrue(Card.reject);
+	}//end alphaSecCodeTest
+	
+	@Test
 	void goodCardTest(Card card) {
+		@SuppressWarnings("unused")
 		Card lastOne = new Card(name, goodCardNumber, goodExpDateTwo, goodSecCode);
 		assertFalse(Card.reject); 
 	}//end goodCardTest
